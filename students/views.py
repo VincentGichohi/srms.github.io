d29f7a0b706613a3933d66bd02335d41dae4ce7d
VincentGichohi <vincentegichohi@gmail.com>
2022-11-11 17:00:58 +0300

    add imports to student views

--- students/views.py
+++ students/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
+from .models import Student
+from .forms import StudentForm
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
