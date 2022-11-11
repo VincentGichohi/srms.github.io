6d41101059cf4070e715d3c73311c6ea6d652690
VincentGichohi <vincentegichohi@gmail.com>
2022-11-11 12:40:55 +0300

    add student models

--- students/models.py
+++ students/models.py
@@ -5,9 +5,21 @@ from datetime import date
 
 
 class Student(models.Model):
-    gender = (
+    GENDER = (
     ('Male', 'Male'),
     ('Female', 'Female'),
     ('Other', 'Other'),
     )
-    
+    student_name = models.CharField(max_length=100)
+    student_roll = models.IntegerField(unique=True)
+    student_email = models.EmailField()
+    student_gender = models.CharField(max_length=8, choices=GENDER)
+    student_class = models.ForeignKey(StudentClass, on_delete=models.CASCADE)
+    student_date_of_birth = models.DateField(auto_now_add=True, auto_now=False)
+
+    def get_absolute_url(self):
+        return reverse('students:student_create')
+
+    def __str__(self):
+        return self.student_name
+        
