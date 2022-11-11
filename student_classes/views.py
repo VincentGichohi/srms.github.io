1999dba3c86588dcf8327f783938ccb974f874be
VincentGichohi <vincentegichohi@gmail.com>
2022-11-11 12:53:03 +0300

    add studentclasscreateview

--- student_classes/views.py
+++ student_classes/views.py
@@ -3,3 +3,13 @@ from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 from .models import StudentClass
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from . import forms
+
+
+
+class StudentClassCreateView(LoginRequiredMixin, CreateView):
+    model = StudentClass
+    form_class = forms.StudentClassForm
+
+    
+
