ca99a5feac1905936a815bb9a6f04a7b2239a6a9
VincentGichohi <vincentegichohi@gmail.com>
2022-11-12 23:19:58 +0300

    add a subjectcreateview and get context data

--- subjects/views.py
+++ subjects/views.py
@@ -9,4 +9,11 @@ from django.urls import reverse_lazy
 class SubjectCreateView(LoginRequiredMixin, CreateView):
     model = Subject
     form_class = SubjectForm
-    
+
+    def get_context_data(self, **kwargs):
+        context =  super(SubjectCreateView, self).get_context_data(**kwargs)
+        context['main_page_title'] = 'Subject Creation'
+        context['panel_name'] = 'Subjects'
+        context['panel_title'] = 'Add a subject'
+        return context
+        
