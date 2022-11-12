b904052a5f74a255880448eee6400cc3613ff6a3
VincentGichohi <vincentegichohi@gmail.com>
2022-11-12 11:03:57 +0300

    add studentcreateview to students app

--- students/views.py
+++ students/views.py
@@ -8,3 +8,13 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 class StudentCreateView(LoginRequiredMixin, CreateView):
     model = Student
+    from_class = StudentForm
+
+    def get_context_data(self, **kwargs):
+        context =  super(StudentCreateView, self).get_context_data(**kwargs)
+        context['amin_page_title'] = 'Student Creation'
+        context['panel_name'] = 'Students'
+        context['panel_title'] = 'Create Student'
+        return context
+        
+
