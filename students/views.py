cc651dfe9b0561fe42e7c9e290e63cf8933d630e
VincentGichohi <vincentegichohi@gmail.com>
2022-11-12 11:37:12 +0300

    refactor imports in student views

--- students/views.py
+++ students/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render
 from django.views.generic import CreateView, ListView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Subject, SubjectCombination
-from .forms import SubjectForm, SubjectCombinationForm
+from .models import Student
+from .forms import StudentForm
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+# Create your views here.
 
 
-class SubjectCreateView(LoginRequiredMixin, CreateView):
-    model = Subject
-    form_class = SubjectForm
+class StudentCreateView(LoginRequiredMixin, CreateView):
+    model = Student
+    form_class = StudentForm
 
     def get_context_data(self, **kwargs):
-        context = super(SubjectCreateView, self).get_context_data(**kwargs)
-        context['main_page_title'] = 'Subject Creation'
-        context['panel_name'] = 'Subjects'
-        context['panel_title'] = 'Add Subject'
+        context = super(StudentCreateView, self).get_context_data(**kwargs)
+        context['main_page_title'] = 'Student Creation'
+        context['panel_name'] = 'Students'
+        context['panel_title'] = 'Create Student'
         return context
 
 
@@ -25,27 +26,26 @@ class StudentListView(LoginRequiredMixin, ListView):
     ]
 
     def get_context_data(self, **kwargs):
-        context =  super().get_context_data(**kwargs)
-        context['main_page_title'] = 'Manage Student'
+        context = super().get_context_data(**kwargs)
+        context['main_page_title'] = 'Manage Students'
         context['panel_name'] = 'Students'
         context['panel_title'] = 'View Students Info'
         context['field_list'] = self.field_list
         return context
 
 
-class StudentUpdateVIew(LoginRequiredMixin, UpdateView):
+class StudentUpdateView(LoginRequiredMixin, UpdateView):
     model = Student
     template_name_suffix = '_form'
     form_class = StudentForm
     success_url = reverse_lazy('students:student_list')
 
     def get_context_data(self, **kwargs):
-        context = super(StudentUpdateVIew, self).get_context_data(**kwargs)
+        context = super(StudentUpdateView, self).get_context_data(**kwargs)
         context['main_page_title'] = 'Update Student Info'
         context['panel_name'] = 'Students'
-        context['panel_title'] = 'Update Student Info'
+        context['panel_title'] = 'Update Student info'
         return context
-
 
 
 class StudentDeleteView(LoginRequiredMixin, DeleteView):
@@ -54,13 +54,8 @@ class StudentDeleteView(LoginRequiredMixin, DeleteView):
     success_url = reverse_lazy('students:student_list')
 
     def get_context_data(self, **kwargs):
-        context =  super(StudentDeleteView, self).get_context_data(**kwargs)
+        context = super(StudentDeleteView, self).get_context_data(**kwargs)
         context['main_page_title'] = 'Student Delete Confirmation'
         context['panel_name'] = 'Students'
         context['panel_title'] = 'Delete Student'
         return context
-
-
-
-
-
