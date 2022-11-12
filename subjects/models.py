e2fe7284a1c804a9b9dd56bea29a511c2eb61834
VincentGichohi <vincentegichohi@gmail.com>
2022-11-12 16:26:17 +0300

    add subjectcombination model and return statements

--- subjects/models.py
+++ subjects/models.py
@@ -20,4 +20,9 @@ class SubjectCombination(models.Model):
     select_class = models.ForeignKey(StudentClass, on_delete=models.CASCADE)
     select_subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
 
-    
+    def get_absolute_url(self):
+        return reverse('subjects:subject_combination_list')
+
+    def __str__(self):
+        return '%s Section-%s'%(self.select_class.class_name, self.select_class.section)
+        
