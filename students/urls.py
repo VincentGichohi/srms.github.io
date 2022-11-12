a4ee839c47cf52a66ce1be76f4d1aed2a49de871
VincentGichohi <vincentegichohi@gmail.com>
2022-11-12 11:26:35 +0300

    add paths to students views

--- students/urls.py
+++ students/urls.py
@@ -4,3 +4,11 @@ from students.views import (
     StudentCreateView, StudentListView, StudentUpdateView, StudentDeleteView
 )
 
+app_name = 'students'
+
+urlpatterns = [
+    path('', StudentListView.as_view(), name='student_list'),
+    path('create/', StudentCreateView.as_view(), name='student_create'),
+    path('update/<int:pk>', StudentUpdateView.as_view(), name='student_update'),
+    path('delete/<int:pk>', StudentDeleteView.as_view(), name='student_delete'),
+]
