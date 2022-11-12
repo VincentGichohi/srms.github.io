18256d83731915e0d03905b64946a068e030d0cb
VincentGichohi <vincentegichohi@gmail.com>
2022-11-12 11:22:14 +0300

    add urls file for students and import views

---
+++ students/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from .views import (
+    SubjectCreateView, SubjectListView, SubjectUpdateView, SubjectDeleteView,
+    SubjectCombinationCreateView, SubjectCombinationListView, SubjectCombinationUpdateView, SubjectCombinationDeleteView
+)
