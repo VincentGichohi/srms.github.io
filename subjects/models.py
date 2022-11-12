98ceb93ce0e19d9a16dab49bd1d35dafc138dddd
VincentGichohi <vincentegichohi@gmail.com>
2022-11-12 16:20:38 +0300

    add subject model

--- subjects/models.py
+++ subjects/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 from django.urls import reverse
 from students.models import StudentClass
+
+
+class Subject(models.Model):
+    subject_name = models.CharField(max_length=100)
+    subject_code = models.IntegerField()
+    subject_creation_date = models.DateTimeField(auto_now=False, auto_now_add=True)
+    subject_update_date = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    def __str__(self):
+        return self.subject_name
+
+    def get_absolute_url(self):
+        return reverse('subjects:subject_list')
+
+        
