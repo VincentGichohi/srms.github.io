3f3ad4ab124444b732ad1234daaf76c19430edd4
VincentGichohi <vincentegichohi@gmail.com>
2022-11-13 10:13:20 +0300

    add customuser and refactor fields

--- user_app/models.py
+++ user_app/models.py
@@ -1,1 +1,17 @@
 from django.db import models
+from .managers import CustomUserManager
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    username = None  # removed , using email instead
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=120)
+    last_name = models.CharField(max_length=120)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.last_name + " " + self.first_name
