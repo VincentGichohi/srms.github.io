52d797ef11ba822f50a2f232f7ab0eb948611c24
VincentGichohi <vincentegichohi@gmail.com>
2022-11-13 10:38:22 +0300

    add imports to user_app views

--- user_app/views.py
+++ user_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .email_backend import EmailBackend
+from django.contrib import messages
+from .forms import CustomUserForm
+from django.contrib import login, logout
 
-# Create your views here.
