78446c2c554bd33e1f3a74bd7a9e26eab78f070a
VincentGichohi <vincentegichohi@gmail.com>
2022-11-13 10:40:19 +0300

    add account register to user_app views

--- user_app/views.py
+++ user_app/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .email_backend import EmailBackend
 from django.contrib import messages
 from .forms import CustomUserForm
 from django.contrib import login, logout
+from django.urls import reverse
+
+
+
+def account_register(request):
+    userForm = CustomUserForm(request.POST or None)
+    context = {
+        'form1': userForm,
+    }
+    if request.method == 'POST':
+        if userForm.is_valid():
+            user = userForm.save(commit=False)
+            user.save()
+            messages.success(request, "Account created. You can login now!")
+            return redirect(reverse('account_login'))
+        else:
+            messages.error(request, "Provided data failed validation")
+            # return account_login(request)
+    return render(request, "voting/registration.html", context)
+
 
