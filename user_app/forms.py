f0a1a29b3e45fdb65e0bb5b920a7b30cc5049fd1
VincentGichohi <vincentegichohi@gmail.com>
2022-11-13 10:16:28 +0300

    add formsettings to forms

--- user_app/forms.py
+++ user_app/forms.py
@@ -2,3 +2,10 @@ from django import forms
 from .models import CustomUser
 from .managers import CustomUserManager
 
+
+class FormSettings(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(FormSettings, self).__init__(*args, **kwargs)
+        # Here make some changes such as:
+        for field in self.visible_fields():
+            field.field.widget.attrs['class'] = 'form-control'
