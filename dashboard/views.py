d7ae71297178c83ec12816a33f57ebd043600849
VincentGichohi <vincentegichohi@gmail.com>
2022-11-13 09:53:29 +0300

    add a results app

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,1 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from xhtml2pdf import pisa
+from django.template.loader import get_template
+from django.http import HttpResponse
+from io import BytesIO
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import PasswordChangeView
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth import authenticate, login
+from django.views.generic import TemplateView, View
+from django.contrib.auth.models import User
+# from results.models
