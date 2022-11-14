2e5fe908f9c999d6426f3f1c9288c5e876a1f398
VincentGichohi <vincentegichohi@gmail.com>
2022-11-14 09:11:30 +0300

    add declareresult model

--- results/models.py
+++ results/models.py
@@ -5,3 +5,13 @@ from django.urls import reverse
 from django.contrib.postgres.fields import JSONField
 
 
+class DeclareResult(models.Model):
+    select_class = models.ForeignKey(StudentClass, on_delete=models.CASCADE)
+    select_student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    marks = JSONField(blank=True)
+
+    def get_absolute_url(self):
+        return reverse('results:declare_result')
+
+    def __str__(self):
+        return "%s Section-%s" % (self.sele~ct_class.class_name, self.select_class.section)
