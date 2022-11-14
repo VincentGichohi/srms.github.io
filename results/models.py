2bcdd168f10e934bb441a3147ded17186d436434
VincentGichohi <vincentegichohi@gmail.com>
2022-11-14 09:06:44 +0300

    add imports to results model

--- results/models.py
+++ results/models.py
@@ -1,2 +1,7 @@
 from django.db import models
-from students.models import StudentClass
+from students.models import Student
+from subjects.models import StudentClass
+from django.urls import reverse
+from django.contrib.postgres.fields import JSONField
+
+
