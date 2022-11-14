e90abdf350d027f8e0c2ba1125010996e3e60d84
VincentGichohi <vincentegichohi@gmail.com>
2022-11-14 09:17:22 +0300

    add declareresult form

--- results/forms.py
+++ results/forms.py
@@ -3,4 +3,15 @@ from django.forms import ModelForm
 from .models import DeclareResult
 
 
-
+class DeclareResultForm(ModelForm):
+    class Meta:
+        model = DeclareResult
+        fields = ['select_class', 'select_student']
+        widgets = {
+            'select_class': forms.Select(attrs={'class': 'form-control'}),
+            'select_student': forms.Select(attrs={'class': 'form-control'}),
+        }
+        labels = {
+            'select_clas': 'Class',
+            'select_student': 'Select Student'
+        }
