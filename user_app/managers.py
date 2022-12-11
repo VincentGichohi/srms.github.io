d9464e03c170d2bf7fa9b29efd880d11c2f5371e
VincentGichohi <vincentegichohi@gmail.com>
2022-12-11 08:31:56 +0300

    comment out managers file for user_app

--- user_app/managers.py
+++ user_app/managers.py
@@ -1,28 +1,4 @@
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager, BaseUserManager
-from django.contrib.auth.hashers import make_password
-from .models import CustomUser
+# from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager, BaseUserManager
+# from user_app.models import CustomUser
 
-class CustomUserManager(BaseUserManager):
 
-    def _create_user(self, email, password, **extra_fields):
-        email = self.normalize_email(email)
-        user = CustomUser(email=email, **extra_fields)
-        user.password = make_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_user(self, email, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", False)
-        extra_fields.setdefault("is_superuser", False)
-        return self._create_user(email, password, **extra_fields)
-
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-        extra_fields.setdefault("user_type", 1)
-        extra_fields.setdefault("last_name", "System")
-        extra_fields.setdefault("first_name", "Administrator")
-
-        assert extra_fields['is_staff']
-        assert extra_fields['is_superuser']
-        return self._create_user(email, password, **extra_fields)
