b6e17086627f466f0b09eda7780d2e44fc2d2335
VincentGichohi <vincentegichohi@gmail.com>
2022-12-11 08:34:05 +0300

    reformat views for user_app by commenting them

--- user_app/views.py
+++ user_app/views.py
@@ -1,60 +1,60 @@
-from django.shortcuts import render, redirect
-from .email_backend import EmailBackend
-from django.contrib import messages
-from .forms import CustomUserForm
-from django.contrib import login, logout
-from django.urls import reverse
+# from django.shortcuts import render, redirect
+# from .email_backend import EmailBackend
+# from django.contrib import messages
+# from .forms import CustomUserForm
+# from django.contrib.auth import login, logout
+# from django.urls import reverse
 
 
 
-def account_register(request):
-    userForm = CustomUserForm(request.POST or None)
-    context = {
-        'form1': userForm,
-    }
-    if request.method == 'POST':
-        if userForm.is_valid():
-            user = userForm.save(commit=False)
-            user.save()
-            messages.success(request, "Account created. You can login now!")
-            return redirect(reverse('account_login'))
-        else:
-            messages.error(request, "Provided data failed validation")
-            # return account_login(request)
-    return render(request, "voting/registration.html", context)
+# def account_register(request):
+#     userForm = CustomUserForm(request.POST or None)
+#     context = {
+#         'form1': userForm,
+#     }
+#     if request.method == 'POST':
+#         if userForm.is_valid():
+#             user = userForm.save(commit=False)
+#             user.save()
+#             messages.success(request, "Account created. You can login now!")
+#             return redirect(reverse('account_login'))
+#         else:
+#             messages.error(request, "Provided data failed validation")
+#             # return account_login(request)
+#     return render(request, "voting/registration.html", context)
 
 
-def account_login(request):
-    if request.user.is_authenticated:
-        if request.user.user_type == '1':
-            return redirect(reverse("adminDashboard"))
-        else:
-            return redirect(reverse("voterDashboard"))
+# def account_login(request):
+#     if request.user.is_authenticated:
+#         if request.user.user_type == '1':
+#             return redirect(reverse("adminDashboard"))
+#         else:
+#             return redirect(reverse("voterDashboard"))
 
-    context = {}
-    if request.method == 'POST':
-        user = EmailBackend.authenticate(request, username=request.POST.get(
-            'email'), password=request.POST.get('password'))
-        if user != None:
-            login(request, user)
-            if user.user_type == '1':
-                return redirect(reverse("adminDashboard"))
-            else:
-                return redirect(reverse("voterDashboard"))
-        else:
-            messages.error(request, "Invalid details")
-            return redirect("/")
+#     context = {}
+#     if request.method == 'POST':
+#         user = EmailBackend.authenticate(request, username=request.POST.get(
+#             'email'), password=request.POST.get('password'))
+#         if user != None:
+#             login(request, user)
+#             if user.user_type == '1':
+#                 return redirect(reverse("adminDashboard"))
+#             else:
+#                 return redirect(reverse("voterDashboard"))
+#         else:
+#             messages.error(request, "Invalid details")
+#             return redirect("/")
 
-    return render(request, "voting/login.html", context)
+#     return render(request, "voting/login.html", context)
 
 
-def account_logout(request):
-    user = request.user
-    if user.is_authenticated:
-        logout(request)
-        messages.success(request, "Thank you for visiting us!")
-    else:
-        messages.error(
-            request, "You need to be logged in to perform this action")
+# def account_logout(request):
+#     user = request.user
+#     if user.is_authenticated:
+#         logout(request)
+#         messages.success(request, "Thank you for visiting us!")
+#     else:
+#         messages.error(
+#             request, "You need to be logged in to perform this action")
 
-    return redirect(reverse("account_login"))
+#     return redirect(reverse("account_login"))
